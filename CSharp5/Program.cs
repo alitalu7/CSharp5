6994b42b029693b2532ce4f57940a50f2ede971f
alitalu7 <55026802+alitalu7@users.noreply.github.com>
2022-06-23 21:54:22 +0300

    Add project files.

---
+++ CSharp5/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Bir sayı giriniz...");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a<20)
+{
+    Console.WriteLine("Girilen sayi yirmiden küçüktür...");
+}
+else if (a == 20)
+{
+    Console.WriteLine("Sayi yirmiye eşittir...");
+}
+else
+{
+    Console.WriteLine("Sayi yirmiden büyüktür...");
+}
